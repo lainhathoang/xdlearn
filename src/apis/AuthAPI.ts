bedb5cec2f81fc58b11f010b83d3db230d29bcd3
lainhathoang <nhathoang8c01@gmail.com>
2024-01-15 23:50:20 +0700

    done signin, singup, storage user's data

--- src/apis/AuthAPI.ts
+++ src/apis/AuthAPI.ts
@@ -1,14 +1,9 @@
 import axios, { AxiosError } from "axios";
 import APIManager from "./APiManager";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { storeObjectData, storeStringData } from "../utils/utils";
 
-type Signup = {
-  name: string;
-  email: string;
-  password: string;
-  role: string;
-};
-
-export const userSignup = async (data: Signup) => {
+export const signup = async (data: SignUp) => {
   try {
     const results = await APIManager("/auth/register", {
       method: "POST",
@@ -19,13 +14,31 @@ export const userSignup = async (data: Signup) => {
     });
     return results;
   } catch (error) {
-    if (error instanceof AxiosError) {
-      // console.log("response: ", error.response?.status);
-      // console.error("data: ", error.response?.data);
-      //   return error.response?.data;
-      // return error.response?.data;
-      return error;
-    }
+    // if (error instanceof AxiosError) {
+    // console.log("response: ", error.response?.status);
+    // console.error("data: ", error.response?.data);
+    //   return error.response?.data;
+    // return error.response?.data;
+    //  return error;
+    // }
+    return error;
+  }
+};
+
+export const signin = async (data: SignIn) => {
+  try {
+    const result = await APIManager("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    });
+    // store data
+    await storeObjectData("user", result.data.user);
+    await storeStringData("token", result.data.token);
+    return result.data;
+  } catch (error) {
     return error;
   }
 };
